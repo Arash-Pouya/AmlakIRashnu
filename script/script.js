b1403fe2878c3be1c8177fe2d230c9cc68f89dbd
Arash-Pouya <arash.pouya.c@gmail.com>
2023-12-12 18:49:50 +0330

    SingInPage

--- script/script.js
+++ script/script.js
@@ -33,3 +33,20 @@ function enablecheckbox() {
     text3.setAttribute("disabled", "");
   }
 }
+
+////////////////////////////////////////////////////
+const slideBoard = document.querySelector(".sliding-board");
+const singUp = document.querySelector(".singUp");
+const singIn = document.querySelector(".singIn");
+
+const slidingState = document.querySelector(".main");
+
+singIn.addEventListener("click", function () {
+  slideBoard.classList.add("sliding");
+  slidingState.classList.replace("sing-up", "sing-in");
+});
+
+singUp.addEventListener("click", function () {
+  slideBoard.classList.remove("sliding");
+  slidingState.classList.replace("sing-in", "sing-up");
+});
