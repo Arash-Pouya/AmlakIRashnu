07c3771b2fc52b49f5fd61342987f249c641846e
Arash-Pouya <arash.pouya.c@gmail.com>
2024-01-04 22:54:31 +0330

    edit

--- script/script.js
+++ script/script.js
@@ -12,25 +12,42 @@
 // };
 
 function enablecheckbox() {
+  text0 = document.getElementById("forradio02");
   text = document.getElementById("forradio03");
   text2 = document.getElementById("forradio04");
+  text01 = document.getElementById("forradio002");
   text3 = document.getElementById("forradio003");
   text4 = document.getElementById("forradio004");
+
+  if (document.getElementById("flexRadioDefault01").checked) {
+    text0.removeAttribute("disabled", "");
+    text.setAttribute("disabled", "");
+    text2.setAttribute("disabled", "");
+  }
   if (document.getElementById("flexRadioDefault03").checked) {
     text.removeAttribute("disabled", "");
     text2.setAttribute("disabled", "");
+    text0.setAttribute("disabled", "");
   }
   if (document.getElementById("flexRadioDefault04").checked) {
     text2.removeAttribute("disabled", "");
     text.setAttribute("disabled", "");
+    text0.setAttribute("disabled", "");
+  }
+  if (document.getElementById("flexRadioDefault001, flexRadioDefault002").checked) {
+    text01.removeAttribute("disabled", "");
+    text3.setAttribute("disabled", "");
+    text4.setAttribute("disabled", "");
   }
   if (document.getElementById("flexRadioDefault003").checked) {
     text3.removeAttribute("disabled", "");
     text4.setAttribute("disabled", "");
+    text01.setAttribute("disabled", "");
   }
   if (document.getElementById("flexRadioDefault004").checked) {
     text4.removeAttribute("disabled", "");
     text3.setAttribute("disabled", "");
+    text01.setAttribute("disabled", "");
   }
 }
 
